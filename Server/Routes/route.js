9080f74f01a0e68860ed39d9abd50edac7df9c1d
developer001 <developer001@rizsolutions.com>
2023-09-24 18:19:46 +0500

    git initial commit

--- Server/Routes/route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addUser } from "../Controller/userController.js";
-
-const Route = express.Router();
-
-Route.post("/add", addUser);
-
-export default Route;

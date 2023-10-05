5a808f5c3a04983ea16ad2a0023391b87c366817
developer001 <developer001@rizsolutions.com>
2023-10-05 14:45:22 +0500

    Remove server/backend folder

--- Server/Routes/route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addUser, getUsers } from "../Controller/userController.js";
-
-const Route = express.Router();
-
-Route.post("/add", addUser);
-Route.get("/users", getUsers);
-
-export default Route;

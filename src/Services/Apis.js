a751f320b89c0bbc9947f6d2e9e70b520b527a85
developer001 <developer001@rizsolutions.com>
2023-10-06 17:30:03 +0500

    changing the url of backend

--- src/Services/Apis.js
+++ src/Services/Apis.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const url = "http://localhost:8000";
+// const url = "http://localhost:8000";
+const url = "https://rizchatbackend.onrender.com";
+
 export const AddUser = async (data) => {
   let add = await axios.post(`${url}/add`, data);
   console.log("add", add);

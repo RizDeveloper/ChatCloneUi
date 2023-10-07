3930679c0156f9a0d2cd68e86059e62576f83456
developer001 <developer001@rizsolutions.com>
2023-10-07 18:33:49 +0500

    deploying bug fixed

--- src/Services/Apis.js
+++ src/Services/Apis.js
@@ -2,15 +2,14 @@ import axios from "axios";
 import { backendUrl } from "../Constants/Data";
 
 // const url = "http://localhost:8000";
-const backendUrl = "https://rizchatbackend.onrender.com";
 
 export const AddUser = async (data) => {
-  let add = await axios.post(`${url}/add`, data);
+  let add = await axios.post(`${backendUrl}/add`, data);
   console.log("add", add);
 };
 
 export const getusers = async () => {
-  let response = await axios.get(`${url}/users`);
+  let response = await axios.get(`${backendUrl}/users`);
   console.log("get", response);
   return response;
 };
